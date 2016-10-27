b9d6691d70826c591f15f3d3d09cdfd805544348
Oscar Tellez <oscartzgz@gmail.com>
2016-10-27 18:10:16 -0500

    Se crean/completan las relaciones entre modelos

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,2 +1,4 @@
 class Contributor < ApplicationRecord
+  has_many :project_contributors
+  has_many :projects, :through => :project_contributors
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :client
+  has_many :project_paiments
+  has_many :project_contributors
+  has_many :contributors, :through => :project_contributors
 end
