5cab322b6653d380cb677bace911606de9e276e1
Oscar Tellez <oscartzgz@gmail.com>
2016-10-28 18:17:19 -0500

    Se configura Nested Resources en las rutas entre el modelo Client y Project

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   resources :project_payments
   resources :projects
   resources :contributors
-  resources :clients
+  resources :clients do
+    resources :projects
+  end
   resources :company_details
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
