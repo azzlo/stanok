21252a06b70dce4e52611d427b2108fe9c9bf6b2
Oscar Tellez <oscartzgz@gmail.com>
2016-11-04 14:35:31 -0600

    Se cambian los valores que se muestra en el index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @runing_projects = Project.where(project_status_id: 2).order(created_at: :desc)
-    @completed_projects = Project.where(project_status_id: 4).order(created_at: :desc)
+    @stoped_projects = Project.where(project_status_id: 1).order(created_at: :desc)
+    @running_projects = Project.where(project_status_id: 2).order(created_at: :desc)
     @paused_projects = Project.where(project_status_id: 3).order(created_at: :desc)
+    @completed_projects = Project.where(project_status_id: 4).order(created_at: :desc)
     @projects = Project.all
   end
 end
